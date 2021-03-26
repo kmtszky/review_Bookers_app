e36a6f4b3301ae2a81ef17519adf9ef63ed67814
kmtszky <kmtszk711@gmail.com>
2021-03-26 16:11:08 +0000

    [update] indexへエラーメッセージ, validation実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,9 +9,13 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id), notice: "Book was successfully created."
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(book.id), notice: "Book was successfully created."
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def edit
